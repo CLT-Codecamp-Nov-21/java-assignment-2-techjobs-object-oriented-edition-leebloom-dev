1d54e19940ae877c3031cdbc83d4f80af338e365
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-02-09 21:58:17 -0500

    Task 4 - Unit Test - Empty Constructor, equals Method, Full Constructor

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,71 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job3 = new Job(
+                "Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence")
+        );
+
+        assertTrue(job3 instanceof Job);
+        assertEquals("Product tester", job3.getName());
+        assertTrue(job3 instanceof Job);
+        assertEquals("ACME", job3.getEmployer().getValue());
+        assertTrue(job3 instanceof Job);
+        assertEquals("Desert", job3.getLocation().getValue());
+        assertTrue(job3 instanceof Job);
+        assertEquals("Quality control", job3.getPositionType().getValue());
+        assertTrue(job3 instanceof Job);
+        assertEquals("Persistence", job3.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job job4 = new Job(
+                "Software Engineer",
+                new Employer("Qualcomm"),
+                new Location("Tokyo"),
+                new PositionType("Front End"),
+                new CoreCompetency("JavaScript")
+        );
+
+        Job job5 = new Job(
+                "Software Engineer",
+                new Employer("Qualcomm"),
+                new Location("Tokyo"),
+                new PositionType("Front End"),
+                new CoreCompetency("JavaScript")
+        );
+
+        assertFalse(job4 == job5);
+    }
+
 
 }
